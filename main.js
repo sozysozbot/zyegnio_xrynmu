437039e04bd8625575fe66aca06ef724d6e9ea9d
sozysozbot <sozysozbot@gmail.com>
2017-06-12 22:54:41 +0900

    switch order

--- main.js
+++ main.js
@@ -80,8 +80,8 @@ function foo(isBase, main_v,kaihom_arr,end)
 function getRow(isBase, end)
 {
 	return '<tr>'+'<td>-'+end+'</td>' +
-		foo(isBase,'a', ['','r','i','u','ru','y'],end) +
 		foo(isBase,'e', ['','r','i','ri','u','ru','y', 'ry'],end) +
+		foo(isBase,'a', ['','r','i','u','ru','y'],end) +
 		foo(isBase,'∅', ['i','ri','y','ry'],end) +
 		foo(isBase,'o', ['','r','i','y'],end) +
 		foo(isBase,'u', ['u','y'],end) +
@@ -95,8 +95,8 @@ function generate(id,isBase){
 	var str='<table id="'+id+'" border="1">\
 	<tr>\
 		<td></td>\
-		<td colspan="6"><span class="main">a</span></td>\
 		<td colspan="8"><span class="main">e</span></td>\
+		<td colspan="6"><span class="main">a</span></td>\
 		<td colspan="4"><span class="kaihom">介i</span>+<span class="main">主∅</span></td>\
 		<td colspan="4"><span class="main">o</span></td>\
 		<td colspan="2"><span class="kaihom">介u</span>+<span class="main">主u</span></td>\
@@ -107,8 +107,8 @@ function generate(id,isBase){
 
 
 	str+=
-		getKaihomRow(['0','r','i','u','ru','y']) +
 		getKaihomRow(['0','r','i','ri','u','ru','y', 'ry']) +
+		getKaihomRow(['0','r','i','u','ru','y']) +
 		getKaihomRow(['i','ri','y','ry']) +
 		getKaihomRow(['0','r','i','y']) +
 		getKaihomRow(['u','y']) +
